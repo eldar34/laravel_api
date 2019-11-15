a6539a9faaa404b16cf162ccdf2517106116ba1a
eldar <eldarphp@ya.ru>
2019-11-15 11:48:46 +0300

    Changed API view

---
+++ resources/views/apiauth/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <example-component></example-component>
+    <passport-clients></passport-clients>
+    <passport-authorized-clients></passport-authorized-clients>
+    <passport-personal-access-tokens></passport-personal-access-tokens>
+
+@endsection
